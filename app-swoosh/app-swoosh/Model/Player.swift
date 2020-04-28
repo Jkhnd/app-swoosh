0218fe049c8a80dbf497a51276ad5e820b3c46f8
Jose Nejad <jOsE@Joses-MacBook-Pro.local>
2020-04-29 00:12:34 +0100

    IBActions

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jose Nejad on 28/04/2020.
+//  Copyright © 2020 JKHND. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
